8068fd59a3fce47757fe1e9baef24779c58e0299
Dierk Koenig <dierk.koenig@canoo.com>
2016-10-23 16:31:24 +0200

    minor beautification

--- src/clock.js
+++ src/clock.js
@@ -1,7 +1,9 @@
 // const now = require("../lib/time.js");
 
 const showtime = function() {
-    document.getElementById("clock").innerHTML = "Current time is " + now().split(",")[1];
+    document
+        .getElementById("clock")
+        .innerHTML = "Current time is " + now().split(",")[1];
 }
 
 showtime()
--- src/today.js
+++ src/today.js
@@ -1,3 +1,5 @@
 // const now = require("../lib/time.js");
 
-document.getElementById("today").innerHTML = "Today is " + now().split(",")[0];
+document
+    .getElementById("today")
+    .innerHTML = "Today is " + now().split(",")[0];
